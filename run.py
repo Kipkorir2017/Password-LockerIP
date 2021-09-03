42a1ed09b3fe404736482a268b1c62b9133730fd
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-03 16:40:56 +0300

    add a function to check if user exist and return boolean value in run file

--- run.py
+++ run.py
@@ -28,3 +28,14 @@ def find_user(username, password):
     function to find user if it exist return account
     '''
     return User.find_by_name(username,password)
+
+#function to check if user exist and return boolean value
+
+def check_existing_user(username, password):
+    '''
+    Function to check if a user exist with username and password and returns a Boolean
+    '''
+    return User.user_exist(username, password)
+
+#Credential Functions
+
