1174d6159eca8b6513f74a49e2e497b234400395
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-03 16:13:17 +0300

    add function to create user in run file

--- run.py
+++ run.py
@@ -3,3 +3,14 @@ from user import User
 from credential import Credential
 import random
 import string
+
+
+#User class Functions
+
+def create_user():
+    '''
+    Function to create new account
+    '''
+    new_user=User("username","password")
+    return new_user
+
